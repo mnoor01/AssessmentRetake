dee8606e86857ab007912a7ae545c4a8e83512ff
mnoor01 <MuhaimenNoor@ac.c4q.nyc>
2018-01-16 18:54:20 -0500

    final commit

--- app/src/main/java/com/example/c4q/reviewforexam/ListAdapter.java
+++ app/src/main/java/com/example/c4q/reviewforexam/ListAdapter.java
@@ -1,6 +1,8 @@
 package com.example.c4q.reviewforexam;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
@@ -23,16 +25,18 @@ public class ListAdapter extends RecyclerView.Adapter<ListViewHolder> {
 
     @Override
     public ListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View childView= LayoutInflater.from(parent.getContext()).inflate(R.layout.number_itemview,parent,false);
+        return new ListViewHolder(childView);
     }
 
     @Override
     public void onBindViewHolder(ListViewHolder holder, int position) {
+        holder.onBind(position);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return integerList.size();
     }
 }
--- app/src/main/java/com/example/c4q/reviewforexam/ListViewHolder.java
+++ app/src/main/java/com/example/c4q/reviewforexam/ListViewHolder.java
@@ -2,13 +2,27 @@ package com.example.c4q.reviewforexam;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by c4q on 1/15/18.
  */
 
 public class ListViewHolder extends RecyclerView.ViewHolder {
+    private ListFragmentPresenter presenter;
+    private TextView textView;
     public ListViewHolder(View itemView) {
         super(itemView);
+        textView=itemView.findViewById(R.id.numberText);
+        presenter=new ListFragmentPresenter();
+    }
+    public void onBind(final int number){
+        textView.setText(number);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.onNumberCLicked(number);
+            }
+        });
     }
 }
